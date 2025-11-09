2ffbfa61818f525d7ca32e8acccc8f7ea21d085c
akashjpal <palgayatrij786@gmail.com>
2025-11-09 21:35:52 +0530

    task/ai-mcq-generator-api - added file upload endpoint

--- question-generator-api/index.js
+++ question-generator-api/index.js
@@ -5,15 +5,38 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.raw({
+  type: [
+    'application/pdf', // PDFs
+    'application/octet-stream', // Word docs
+  ],
+  limit: '10mb'
+ }));
 
 app.get("/", (req, res) => {
-    res.send("Question Generator API is running.");
+  res.send("Question Generator API is running.");
+});
+
+app.post("/file-upload", (req, res) => {
+  const filename = req.headers["x-filename"];
+  const fileSize = req.headers["content-length"] / 1_000_000;
+  const fileBuffer = req.body;
+  console.log(fileSize);
+  console.log(req.headers);
+  console.log(fileBuffer);
+  if(!fileBuffer && fileBuffer.length === 0) {
+    return res.status(400).json({ error: "No file uploaded." });
+  }
+  console.log("✅ Received filename:", filename);
+  console.log("📦 File size:", fileSize, "MB");
+
+  res.json({ message: "File received", filename });
 });
 
 app.get("/health", (req, res) => {
-    res.json({ status: "ok" });
+  res.json({ status: "ok" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
