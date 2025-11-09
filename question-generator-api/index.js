11cc17179dfb348879f753a2d2ee49062248503d
akashjpal <palgayatrij786@gmail.com>
2025-11-09 23:23:16 +0530

    task/ai-mcq-generator-api - add fileuploader service to the appwrite

--- question-generator-api/index.js
+++ question-generator-api/index.js
@@ -1,36 +1,43 @@
 import express from "express";
+import dotenv from "dotenv";
+import { FileUploader } from "./helpers/fileUploader.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.raw({
-  type: [
-    'application/pdf', // PDFs
-    'application/octet-stream', // Word docs
-  ],
-  limit: '10mb'
- }));
+app.use(
+  express.raw({
+    type: [
+      "application/pdf", // PDFs
+      "application/octet-stream", // Word docs
+    ],
+    limit: "10mb",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Question Generator API is running.");
 });
 
-app.post("/file-upload", (req, res) => {
-  const filename = req.headers["x-filename"];
-  const fileSize = req.headers["content-length"] / 1_000_000;
-  const fileBuffer = req.body;
-  console.log(fileSize);
-  console.log(req.headers);
-  console.log(fileBuffer);
-  if(!fileBuffer && fileBuffer.length === 0) {
-    return res.status(400).json({ error: "No file uploaded." });
+app.post("/file-upload", async (req, res) => {
+  try {
+    const filename = req.headers["x-filename"];
+    const fileSize = req.headers["content-length"] / 1_000_000;
+    const fileBuffer = req.body;
+    if (!fileBuffer && fileBuffer.length === 0) {
+      return res.status(400).json({ error: "No file uploaded." });
+    }
+    const uploader = new FileUploader();
+    await uploader.storeFile(fileBuffer, filename);
+    console.log("✅ Received filename:", filename);
+    console.log("📦 File size:", fileSize, "MB");
+    res.json({ message: "File received", filename });
+  } catch (error) {
+    console.error("❌ Error uploading file:", error);
+    res.status(500).json({ error: "Failed to upload file." });
   }
-  console.log("✅ Received filename:", filename);
-  console.log("📦 File size:", fileSize, "MB");
-
-  res.json({ message: "File received", filename });
 });
 
 app.get("/health", (req, res) => {
